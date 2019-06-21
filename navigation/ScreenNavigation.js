7b09497543c4c63eb155c3de28c0b8e25cc6fa03
Michal Szmyd <michal.szmyd@icloud.com>
2019-06-21 16:58:01 +0200

    Added icons for bottom navigator

--- navigation/ScreenNavigation.js
+++ navigation/ScreenNavigation.js
@@ -2,13 +2,44 @@ import {
   createBottomTabNavigator,
   createAppContainer
 } from 'react-navigation';
+import Icon from 'react-native-vector-icons/dist/Feather';
+import React from 'react';
 import { HomeNavigator, CategoryNavigator, SearchNavigator, CartNavigator } from './StackNavigators';
 
 const BottomNavigator = createBottomTabNavigator({
-  Home: { screen: HomeNavigator },
-  Category: CategoryNavigator,
-  Search: SearchNavigator,
-  Cart: { screen: CartNavigator },
+  Home: {
+    screen: HomeNavigator,
+    navigationOptions: {
+      tabBarIcon: ({ tintColor }) => (
+        <Icon size={19} color={tintColor} name="home" />
+      )
+    }
+  },
+  Category: {
+    screen: CategoryNavigator,
+    navigationOptions: {
+      title: 'Categories',
+      tabBarIcon: ({ tintColor }) => (
+        <Icon size={19} color={tintColor} name="list" />
+      )
+    }
+  },
+  Search: {
+    screen: SearchNavigator,
+    navigationOptions: {
+      tabBarIcon: ({ tintColor }) => (
+        <Icon size={19} color={tintColor} name="search" />
+      )
+    }
+  },
+  Cart: {
+    screen: CartNavigator,
+    navigationOptions: {
+      tabBarIcon: ({ tintColor }) => (
+        <Icon size={19} color={tintColor} name="shopping-cart" />
+      )
+    }
+  },
 })
 
 const ScreenNavigation = createAppContainer(BottomNavigator);
